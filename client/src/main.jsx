2f8af7c38c4e7aedbb8d530c04b6ae08b7293a0e
Brock Altug <brock.altug99@gmail.com>
2025-01-17 23:18:49 -0800

    Set up React Router with dynamic routing and nested routes
    - Configured React Router with 'createBrowserRouter' and 'RouterProvider'.
    - Added routes for Home, Signup, Login, Profile, SinglePost, and ErrorPage.
    - Implemented dynamic routing for user profile and single post pages with parameters.
    - Set up 'ErrorPage' as the fallback for invalid routes.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.jsx';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import SinglePost from './pages/SinglePost';
+import Profile from './pages/Profile';
+import ErrorPage from './pages/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, 
+    errorElement: <ErrorPage />, 
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/profiles/:username',
+        element: <Profile />
+      }, {
+        path: '/me',
+        element: <Profile />
+      }, {
+        path: '/posts/:postId',
+        element: <SinglePost />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
