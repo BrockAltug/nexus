6b513e06561b71cc40c9156deb2d8be68253d622
Brock Altug <brock.altug99@gmail.com>
2025-01-17 23:20:39 -0800

    Add GraphQL mutations for user, post, and comment management
    - Created LOGIN_USER mutation to log in a user and return token and user data.
    - Added ADD_USER mutation for adding a new user with token and user data.
    - Created ADD_POST mutation to add a new post with its details and comments.
    - Added ADD_COMMENT mutation to add comments to posts.
    - Created REMOVE_POST mutation to delete a post by its ID.
    - Added REMOVE_COMMENT mutation to remove comments from a post.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,87 @@
+import { gql } from '@apollo/client';
+
+// Mutation to log in a user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Mutation to add a new user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Mutation to add a new post
+export const ADD_POST = gql`
+  mutation addPost($postText: String!) {
+    addPost(postText: $postText) {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+// Mutation to add a comment to a post
+export const ADD_COMMENT = gql`
+  mutation addComment($postId: ID!, $commentText: String!) {
+    addComment(postId: $postId, commentText: $commentText) {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+// Mutation to remove a post
+export const REMOVE_POST = gql`
+  mutation removePost($postId: ID!) {
+    removePost(postId: $postId) {
+      _id
+      postText
+    }
+  }
+`;
+
+// Mutation to remove a comment from a post
+export const REMOVE_COMMENT = gql`
+  mutation removeComment($postId: ID!, $commentId: ID!) {
+    removeComment(postId: $postId, commentId: $commentId) {
+      _id
+      postText
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
